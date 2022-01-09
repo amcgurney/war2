1650573ab8cb5f17357990916d183f9dbd2c4344
Alan McGurney <mcgurneyalan@gmail.com>
2022-01-09 18:21:22 -0500

    putting this on github

---
+++ main.js
@@ -0,0 +1,71 @@
+const suits = ["♥", "♦", "♠", "♣"]
+const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+let playerCard, computerCard;
+const computerCardSlot = document.querySelector(".computer-card-slot");
+const playerCardSlot = document.querySelector(".player-card-slot");
+let computerDeck = document.querySelector(".computer-deck");
+let playerDeck = document.querySelector(".player-deck");
+const text = document.querySelector(".text");
+const startButton = document.querySelector(".start-button");
+const dealButton = document.querySelector(".deal-button");
+const playRoundButton = document.querySelector(".play-round-button");
+const deck = [];
+let playerPile = [], computerPile = [];
+let playerDiscardPile = [], computerDiscardPile = [];
+
+//console.log(CARD_VALUE_MAP) 
+
+startButton.addEventListener("click", startGame);  
+dealButton.addEventListener("click", dealCards);
+
+playRoundButton.addEventListener("click", playRound);
+
+
+
+function startGame() {
+    for(let i = 0; i < suits.length; i++) {
+        for(let j = 0; j < ranks.length; j++) {
+            deck.push({
+                suit: suits[i],
+                rank: ranks[j],
+                value: j +2            
+            })
+        }
+    } 
+    shuffle(), dealCards(), playRound();
+}
+startGame();
+
+function playRound() {
+        playerCard = playerPile.pop()
+        computerCard = computerPile.pop()
+        playerCardSlot.innerHTML = playerCard.suit + playerCard.rank;
+        computerCardSlot.innerHTML = computerCard.suit + computerCard.rank;
+        console.log(playerCard, computerCard)
+        winningConditions();
+}
+
+function dealCards() {
+    playerPile = deck.splice(0,26)
+    computerPile = deck
+    // console.log(playerPile)
+    // console.log(computerPile)
+}
+     
+//console.log(dealCards)
+
+function shuffle() {
+    deck.sort((a, b) => Math.random() - .5)
+}
+// console.log(deck)
+
+function winningConditions() {
+    if (playerCard.value > computerCard.value) {
+        text.innerText = "Winner", playerDiscardPile.push(playerCard, computerCard);
+        
+    }
+        else if (playerCard.value < computerCard.value) {
+            text.innerText = "Loser", computerDiscardPile.push(playerCard, computerCard);
+        }
+}   
