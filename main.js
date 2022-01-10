d1861e592377fbce29f8defcd3d1dd7a8093ea77
Alan McGurney <mcgurneyalan@gmail.com>
2022-01-10 10:07:33 -0500

    2nd commit cleaned up

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 const suits = ["♥", "♦", "♠", "♣"]
 const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-let playerCard, computerCard;
 const computerCardSlot = document.querySelector(".computer-card-slot");
 const playerCardSlot = document.querySelector(".player-card-slot");
-let computerDeck = document.querySelector(".computer-deck");
-let playerDeck = document.querySelector(".player-deck");
 const text = document.querySelector(".text");
 const startButton = document.querySelector(".start-button");
 const dealButton = document.querySelector(".deal-button");
 const playRoundButton = document.querySelector(".play-round-button");
 const deck = [];
+let computerDeck = document.querySelector(".computer-deck");
+let playerDeck = document.querySelector(".player-deck");
+let playerCard, computerCard;
 let playerPile = [], computerPile = [];
 let playerDiscardPile = [], computerDiscardPile = [];
 
-//console.log(CARD_VALUE_MAP) 
-
+playRoundButton.addEventListener("click", playRound);
 startButton.addEventListener("click", startGame);  
 dealButton.addEventListener("click", dealCards);
 
-playRoundButton.addEventListener("click", playRound);
 
 
 
